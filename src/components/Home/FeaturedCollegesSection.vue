a7f5598c68c2bc8118860fda9fe5d3e73842fc71
Mayank Goyal <2004mayankgoyal@gmail.com>
2025-12-05 16:50:51 +0530

    college image

--- src/components/Home/FeaturedCollegesSection.vue
+++ src/components/Home/FeaturedCollegesSection.vue
@@ -132,7 +132,6 @@ function goToCollege(college) {
 .college-img {
   width: 100%;
   height: 200px;
-  object-fit: cover;
 }
 
 h3 {
--- src/components/Home/PopularCollege.vue
+++ src/components/Home/PopularCollege.vue
@@ -139,7 +139,7 @@ const sortedColleges = computed(() => {
 .college-img {
   width: 100%;
   height: 200px;
-  object-fit: cover;
+
 }
 
 h3 {
